a30b398fa6ae71051e0d611b05759b6bf914be98
NIRMITHA <jayathissaara@gmail.com>
2021-08-12 20:03:18 +0530

    hey

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,20 @@
+import { React } from 'react';
+import { useParams, Route } from 'react-router-dom';
+
+import Comments from '../Components/comments/Comments';
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;
